ba3345e2e1a9f35b076d026f6ac62a13161e58d2
Muhammad Ali Khan <ak098boy@gmail.com>
2026-08-18 21:12:46 +0500

    fix: allow backend origin

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://reposcribe-r84g.onrender.com",
+  ],
 };
 
 export default nextConfig;
